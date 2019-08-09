a66f7aba934b0296e6f680ea1a8d2a09c60e8171
Kadir <kegilmez@gmail.com>
2019-08-09 17:33:50 +0100

    add primary-secondary and error buttons to cyf storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Button.css";
 const Button = props => (
-  <button onClick={props.onClick} style={props.style}>
+  <button onClick={props.onClick} className={props.className} style={props.style}>
     {props.label && <span>{props.label}</span>}
   </button>
 );
--- src/stories/buttonStories.js
+++ src/stories/buttonStories.js
@@ -1,13 +1,119 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
+import "./buttonStories.css";
 import Button from "../components/Button";
 
 storiesOf("Button", module)
-  .add("with primary", () => (
-    <Button onClick={action("click")} label="Primary Button" />
+  // primary Button
+  .add("primary", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-primary"
+    />
   ))
+  .add("primary outline", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-primary-outline"
+    />
+  ))
+  .add("primary-light", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-primary-light"
+    />
+  ))
+  .add("primary with gray shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-primary-gray-shadow"
+    />
+  ))
+  .add("primary with shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-primary-shadow"
+    />
+  ))
+  // secondary Button
+  .add("secondary", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-secondary"
+    />
+  ))
+  .add("secondary outline", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-secondary-outline"
+    />
+  ))
+  .add("secondary-light", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-secondary-light"
+    />
+  ))
+  .add("secondary with gray shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-secondary-gray-shadow"
+    />
+  ))
+  .add("secondary with shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-secondary-shadow"
+    />
+  ))
+  // error Button
+  .add("error", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-error"
+    />
+  ))
+  .add("error outline", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-error-outline"
+    />
+  ))
+  .add("error-light", () => (
+    <Button
+      onClick={action("click")}
+      label="Button"
+      className="btn btn-error-light"
+    />
+  ))
+  .add("error with gray shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-error-gray-shadow"
+    />
+  ))
+  .add("error with shadow", () => (
+    <Button
+      label="Button"
+      onClick={action("click")}
+      className="btn btn-error-shadow"
+    />
+  ))
+  //////
   .add("with outline", () => (
     <Button
       label="Outline Button"
