9bee4bb04c7c4693c36fa3bdceebf1f3eaa9c765
Kadir <kegilmez@gmail.com>
2019-08-14 18:28:48 +0100

    menus

---
+++ src/components/LargeMenu/LargeMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./large-menu.css";
+const Menu = props => {
+  const { menuItems } = props;
+  return (
+    <ul className="large-menu">
+      {menuItems.map(menuItem => (
+        <li className="listItem">
+          <a className="large-menuItem" href={menuItem.link}>
+            {menuItem.navItem}
+          </a>
+          <i className="icon">♥️</i>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Menu;
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,2 +1,3 @@
 import "./buttonStories";
 import "./inputStories";
+import "./menuStories"
