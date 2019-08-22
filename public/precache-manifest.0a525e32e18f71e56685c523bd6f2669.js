40654d738f968358cf36bb787461648c24b2ddc4
aldeez988 <aldeez911088@gmail.com>
2019-08-22 15:28:58 +0100

    including samatar comments

---
+++ public/precache-manifest.0a525e32e18f71e56685c523bd6f2669.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db136963b76e15a65e919124ecff5074",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.944ada364dfdd619fc44.bundle.js"
+  },
+  {
+    "revision": "e046ada54e3cd2335840",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.944ada364dfdd619fc44.bundle.js"
+  },
+  {
+    "url": "vendors~main.944ada364dfdd619fc44.bundle.js"
+  }
+]);
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import LargeMenu from "./LargeMenu/LargeMenu.js";
+
+import SmallMenu from "./SmallMenu/SmallMenu.js";
+import Button from "./Button";
+import Input from "./Input";
+
+export { LargeMenu, SmallMenu, Input, Button };
