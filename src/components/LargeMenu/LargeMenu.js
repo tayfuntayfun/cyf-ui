e90717405cd012f38da7e3f2a415bd0565231517
aldeez988 <aldeez911088@gmail.com>
2019-09-03 16:00:53 +0100

    changing the large menu to styled component

--- src/components/LargeMenu/LargeMenu.js
+++ src/components/LargeMenu/LargeMenu.js
@@ -1,20 +1,53 @@
 import React from "react";
+import styled from "styled-components";
 import "./large-menu.css";
 
+const LargeMenuWrapper = styled.ul`
+  width: 250px;
+  margin: 0;
+  padding: 0;
+  border: 2px solid #bdbdbd;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.2), 0px 4px 5px rgba(0, 0, 0, 0.12),
+    0px 2px 4px rgba(0, 0, 0, 0.14);
+  font-family: "Roboto", sans-serif;
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  &:hover {
+    background: #eeeeee;
+  }
+`;
+
+const LargeMenuItem = styled.a`
+  text-decoration: none;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 30px;
+  letter-spacing: 0.5px;
+  color: black;
+  padding: 15px;
+`;
+const Icon = styled.i`
+  line-height: 30px;
+  font-size: 36px;
+  padding: 15px;
+`;
 const Menu = props => {
   const { menuItems } = props;
 
   return (
-    <ul className="large-menu">
+    <LargeMenuWrapper>
       {menuItems.map(menuItem => (
-        <li key={menuItem.navItem} className="listItem">
-          <a className="large-menuItem" href={menuItem.link}>
-            {menuItem.navItem}
-          </a>
-          <i className="icon">♥️</i>
-        </li>
+        <ListItem key={menuItem.navItem}>
+          <LargeMenuItem href={menuItem.link}>{menuItem.navItem}</LargeMenuItem>
+          <Icon>♥️</Icon>
+        </ListItem>
       ))}
-    </ul>
+    </LargeMenuWrapper>
   );
 };
 
