e4768d8610a574ab15dd2e60db9b9084a16aadd7
aldeez988 <aldeez911088@gmail.com>
2019-09-04 18:18:26 +0100

    changing the cards to styled components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,68 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+const CardDivider = styled.span`
+  background-color: #e0e0e0;
+  width: 40%;
+`;
 
+const BtnBodyHeaderContainer = styled.div`
+  margin: 5px 0px 20px 20px;
+`;
+
+const Body = styled.p`
+  letter-spacing: 0.5px;
+  font-size: 18px;
+  line-height: 28px;
+`;
+
+const ButtonsContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const renderButtons = (label, variant, onClickHandler) => {
+  return (
+    <Button
+      label={label ? label : "Button"}
+      variant={variant}
+      onClick={onClickHandler ? onClickHandler : () => {}}
+    />
+  );
+};
 const Card = props => {
+  const {
+    divider,
+    src,
+    leftButtonLabel,
+    rightButtonLabel,
+    leftButtonOnClickHandler,
+    rightButtonOnClickHandler,
+  } = props;
   return (
     <article className={props.className} style={props.style}>
-      {props.divider && (
-        <span style={{ backgroundColor: "#E0E0E0", width: "40%" }}>
-          <img />
-        </span>
+      {divider && (
+        <CardDivider>
+          <img src={src ? src : ""} style={{ width: "100%" }} />
+        </CardDivider>
       )}
-      <div className={props.btnBodyHeaderContainer}>
+      <BtnBodyHeaderContainer className={props.btnBodyHeaderContainer}>
         <h1>{props.title}</h1>
-        <p>{props.body}</p>
-        <div className={props.buttonsContainerClass}>
-          {/* <Button
-            label="Button"
-            className="btn-primary-outline"
-            style={{ width: "30%" }}
-          />
-          <Button
-            label="Button"
-            className="btn-primary-outline"
-            style={{ width: "30%" }}
-          /> */}
-        </div>
-      </div>
+        <Body>{props.body}</Body>
+        <ButtonsContainer className={props.buttonsContainerClass}>
+          {renderButtons(
+            leftButtonLabel,
+            "btnPrimaryOutlined",
+            leftButtonOnClickHandler
+          )}
+          {renderButtons(
+            rightButtonLabel,
+            "btnPrimaryOutlined",
+            rightButtonOnClickHandler
+          )}
+        </ButtonsContainer>
+      </BtnBodyHeaderContainer>
     </article>
   );
 };
