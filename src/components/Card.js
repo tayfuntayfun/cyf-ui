5592ca725b663cf43d9222fbea93e4f056cbbdcc
aldeez988 <aldeez911088@gmail.com>
2019-09-04 18:23:40 +0100

    removing the card css file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
+
+const CardWrapper = styled.article`
+  display: flex;
+  align-items: stretch;
+  background: #ffffff;
+  /* 2px - Active Card */
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.2), 0px 3px 4px rgba(0, 0, 0, 0.12),
+    0px 2px 4px rgba(0, 0, 0, 0.14);
+  border-radius: 5px;
+  position: absolute;
+  width: 70%;
+  height: 50%;
+  font-family: "Open Sans", sans-serif;
+`;
 const CardDivider = styled.span`
   background-color: #e0e0e0;
   width: 40%;
@@ -31,6 +45,7 @@ const renderButtons = (label, variant, onClickHandler) => {
     />
   );
 };
+
 const Card = props => {
   const {
     divider,
@@ -41,7 +56,7 @@ const Card = props => {
     rightButtonOnClickHandler,
   } = props;
   return (
-    <article className={props.className} style={props.style}>
+    <CardWrapper>
       {divider && (
         <CardDivider>
           <img src={src ? src : ""} style={{ width: "100%" }} />
@@ -63,7 +78,7 @@ const Card = props => {
           )}
         </ButtonsContainer>
       </BtnBodyHeaderContainer>
-    </article>
+    </CardWrapper>
   );
 };
 
